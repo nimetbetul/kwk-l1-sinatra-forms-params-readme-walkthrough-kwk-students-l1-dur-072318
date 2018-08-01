228fd59a2c7ad624c64ac4955d152f2bb6d844e0
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-08-01 20:27:11 +0000

    Alexina edit

--- app.rb
+++ app.rb
@@ -1,11 +1,32 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :index
+  end
 
-  get '/food_form' do
+  get '/sportsregister' do
     erb :food_form
   end
 
   # Add your post route and action below
 
+post '/food' do
+  @name=params[:name]
+  @what_sport=params[:what_sport]
+  @school=params[:school]
+ # "Hi #{@name}, you've registered for #{@what_sport}. The tryouts are going to be held at #{@school} on Monday at 3:00 pm. See you there!"
+  erb :food
+  end
 end
+
+
+
+
+
+
+
+
+
+
